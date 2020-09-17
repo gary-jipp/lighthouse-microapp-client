6c5ee7c156ff67d4ef40c0020331e82e2eb42c80
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-09-17 07:58:11 -0700

    Update

--- scripts.js
+++ scripts.js
@@ -1,33 +1,38 @@
-function toggleHidden($item) {
-  $item.toggleClass("hidden");
-}
-
 // Stuff to do when page loads
 $(function () {
-
   $("#add").on("click", function (event) {
-    addNewChild($("#container"));
-    $("input").focus();
+
+    getQuotes();
+    $("#count").focus();
+    $("#count").val('');
   });
 
-  $("#clear").on("click", function(event) {
-    $("#container").empty();
-    });
-    
- });
-
-// Add a new list item
-function addNewChild(parent) {
-  const child = $("<li>");
- 
-  // Calling our own REST service
-  const url = "http://localhost:8080/api/kanye";
-  
-  $.get(url, function (data) {
-    // console.log(data);
-    child.text(data.quote);
-    parent.append(child);
+  $("#clear").on("click", function (event) {
+    $("#container").empty(); // This is how we clear "content" (the stuff between tags)
+    $("#count").val('');  // This is how we empty an <input>
   });
+});
 
+// Notice we can define functions in this format as well
+const renderQuotes = function (quotes) {
+  const container = $("#container");
 
-}
+  // Create an <li> item for each quotes and add to our container
+  for (const quote of quotes) {
+    const item = `<li>${quote}</li>`;
+    container.prepend(item);  // Adds to the beginning
+  }
+};
+
+// Get some new quotes
+function getQuotes() {
+
+  // Get the count as the value of the input field
+  const count = $("#count").val();
+
+  // Call external REST service with our count (that's what this url expects)
+  const url = `http://ron-swanson-quotes.herokuapp.com/v2/quotes/${count}`;
+
+  // jQuery ajax
+  $.get(url, renderQuotes);
+};
