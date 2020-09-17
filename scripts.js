fd39e90d6385435b718da385626c35a4c0f74205
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-09-17 00:03:51 -0700

    Update

--- scripts.js
+++ scripts.js
@@ -1,33 +1,29 @@
-function toggleHidden($item) {
-  $item.toggleClass("hidden");
-}
-
 // Stuff to do when page loads
 $(function () {
-
   $("#add").on("click", function (event) {
-    addNewChild($("#container"));
+    getQuote();
     $("input").focus();
   });
 
-  $("#clear").on("click", function(event) {
+  $("#clear").on("click", function (event) {
     $("#container").empty();
-    });
-    
- });
+  });
+});
+
+const renderQuote = function (text) {
+  const item = `<li>${text}</li>`;
+
+  const container = $("#container");
+  container.prepend(item);
+};
 
 // Add a new list item
-function addNewChild(parent) {
-  const child = $("<li>");
-  const text = $("#input").val();
-
+function getQuote() {
   // Calling an external REST service
   const url = "https://api.kanye.rest";
-  
+
   $.get(url, function (data) {
     // console.log(data);
-    child.text(data.quote);
+    renderQuote(data.quote);
   });
-
-  parent.prepend(child);
 }
