60eab535561e94ef31bc46d995f3b75a460e1d0b
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-09-18 13:34:23 -0700

    Update

--- scripts.js
+++ scripts.js
@@ -1,33 +1,4 @@
 // Stuff to do when page loads
 $(function () {
-  $("#add").on("click", function (event) {
-    getQuote();
-  });
 
-  $("#clear").on("click", function (event) {
-    $("#container").empty();
-  });
 });
-
-const renderQuote = function (quote) {
-  const $item = $(`<li>${quote}</li>`);
-  const $container = $("#container");
-  $container.prepend($item);
-};
-
-const renderQuotes = function (quotes) {
-  for (const quote of quotes) {
-    renderQuote(quote);
-  }
-};
-
-// Add a new list item
-const getQuote = function () {
-
-  const data = [
-    "To err is human, to forgive devine",
-    "Some days I really wish I was a cat",
-  ];
-
-  renderQuotes(data);
-};
